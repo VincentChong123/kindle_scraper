5e38b860a5e699b2557f8c2706988bc5d0b7231c
Vincent Chong <ws.chong.sg@gmail.com>
2023-12-04 23:08:30 +0800

    draft1

---
+++ kindle_scrape/screenshot.py
@@ -0,0 +1,10 @@
+#https://github.com/mbrochh/kindle-scrape-summarise
+#https://support.apple.com/en-sg/guide/automator/welcome/mac
+
+from PIL import ImageGrab
+
+def take_screenshot():
+    screenshot.save(filename)
+
+if __name__ == "__main__":
+    take_screenshot()
---
+++ kindle_scrape/summerize.py
@@ -0,0 +1,12 @@
+PROMPT = """
+Your taks is as follow
+
+
+
+
+"""
+
+PROMPT.format(
+
+    
+)
